28fe2be28984d843fb3ee297a46246342fc91ff2
s-as-as100 <s.arifsaifi000@gmail.com>
2021-04-25 03:43:02 +0530

    react carousel package use for different image shown

---
+++ task/src/App.js
@@ -0,0 +1,15 @@
+
+ 
+import './App.css';
+ 
+import Image from './components/Coursel';
+
+function App() {
+  return (
+    <div className="App">
+        <Image/>
+    </div>
+  );
+}
+
+export default App;
